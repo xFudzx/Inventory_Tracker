af925d3a1775126c6c64ae5a950b28983b4dd9c9
Fudail Ahmed <144148102+xFudzx@users.noreply.github.com>
2024-08-10 12:29:36 +0100

    final

--- firebase.js
+++ firebase.js
@@ -5,23 +5,22 @@ import { getFirestore } from "firebase/firestore";
 
 
 
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyArl3gO2J1NHsYusCD2nG6YHOilJl-kB88",
-  authDomain: "inventory-management-92c38.firebaseapp.com",
-  projectId: "inventory-management-92c38",
-  storageBucket: "inventory-management-92c38.appspot.com",
-  messagingSenderId: "487603693527",
-  appId: "1:487603693527:web:c9f08e0f32bdd9a2280d92",
-  measurementId: "G-6HNRZLTYN5"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
-
-export { firestore };
+export {firestore};
